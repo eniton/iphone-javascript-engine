f5f60f2312abba67ec25d4bc372ed5016bc3943f
Wolfgang Knebel <wknebel@numberfour.eu>
2010-04-13 21:47:02 +0200

    Comment out profiler server as it clashes with the one from systems JSCore package

--- profiler/ProfilerServer.mm
+++ profiler/ProfilerServer.mm
@@ -23,93 +23,93 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "config.h"
-#import "ProfilerServer.h"
-
-#import "JSProfilerPrivate.h"
-#import "JSRetainPtr.h"
-
-#import <Foundation/Foundation.h>
-#if PLATFORM(IPHONE_SIMULATOR)
-#import <Foundation/NSDistributedNotificationCenter.h>
-#endif
-
-@interface ProfilerServer : NSObject {
-@private
-    NSString *_serverName;
-    unsigned _listenerCount;
-}
-+ (ProfilerServer *)sharedProfileServer;
-- (void)startProfiling;
-- (void)stopProfiling;
-@end
-
-@implementation ProfilerServer
-
-+ (ProfilerServer *)sharedProfileServer
-{
-    static ProfilerServer *sharedServer;
-    if (!sharedServer)
-        sharedServer = [[ProfilerServer alloc] init];
-    return sharedServer;
-}
-
-- (id)init
-{
-    if (!(self = [super init]))
-        return nil;
-
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    if ([defaults boolForKey:@"EnableJSProfiling"])
-        [self startProfiling];
-
-#if PLATFORM(IPHONE_SIMULATOR)
-    // FIXME: <rdar://problem/6546135>
-    // The catch-all notifications
-    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(startProfiling) name:@"ProfilerServerStartNotification" object:nil];
-    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(stopProfiling) name:@"ProfilerServerStopNotification" object:nil];
-#endif
-
-    // The specific notifications
-    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
-    _serverName = [[NSString alloc] initWithFormat:@"ProfilerServer-%d", [processInfo processIdentifier]];
-
-#if PLATFORM(IPHONE_SIMULATOR)
-    // FIXME: <rdar://problem/6546135>
-    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(startProfiling) name:[_serverName stringByAppendingString:@"-Start"] object:nil];
-    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(stopProfiling) name:[_serverName stringByAppendingString:@"-Stop"] object:nil];
-#endif
-
-    [pool drain];
-
-    return self;
-}
-
-- (void)startProfiling
-{
-    if (++_listenerCount > 1)
-        return;
-    JSRetainPtr<JSStringRef> profileName(Adopt, JSStringCreateWithUTF8CString([_serverName UTF8String]));
-    JSStartProfiling(0, profileName.get());
-}
-
-- (void)stopProfiling
-{
-    if (!_listenerCount || --_listenerCount > 0)
-        return;
-    JSRetainPtr<JSStringRef> profileName(Adopt, JSStringCreateWithUTF8CString([_serverName UTF8String]));
-    JSEndProfiling(0, profileName.get());
-}
-
-@end
+//#import "config.h"
+//#import "ProfilerServer.h"
+//
+//#import "JSProfilerPrivate.h"
+//#import "JSRetainPtr.h"
+//
+//#import <Foundation/Foundation.h>
+//#if PLATFORM(IPHONE_SIMULATOR)
+//#import <Foundation/NSDistributedNotificationCenter.h>
+//#endif
+//
+//@interface ProfilerServer : NSObject {
+//@private
+//    NSString *_serverName;
+//    unsigned _listenerCount;
+//}
+//+ (ProfilerServer *)sharedProfileServer;
+//- (void)startProfiling;
+//- (void)stopProfiling;
+//@end
+//
+//@implementation ProfilerServer
+//
+//+ (ProfilerServer *)sharedProfileServer
+//{
+//    static ProfilerServer *sharedServer;
+//    if (!sharedServer)
+//        sharedServer = [[ProfilerServer alloc] init];
+//    return sharedServer;
+//}
+//
+//- (id)init
+//{
+//    if (!(self = [super init]))
+//        return nil;
+//
+//    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+//
+//    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+//    if ([defaults boolForKey:@"EnableJSProfiling"])
+//        [self startProfiling];
+//
+//#if PLATFORM(IPHONE_SIMULATOR)
+//    // FIXME: <rdar://problem/6546135>
+//    // The catch-all notifications
+//    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(startProfiling) name:@"ProfilerServerStartNotification" object:nil];
+//    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(stopProfiling) name:@"ProfilerServerStopNotification" object:nil];
+//#endif
+//
+//    // The specific notifications
+//    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+//    _serverName = [[NSString alloc] initWithFormat:@"ProfilerServer-%d", [processInfo processIdentifier]];
+//
+//#if PLATFORM(IPHONE_SIMULATOR)
+//    // FIXME: <rdar://problem/6546135>
+//    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(startProfiling) name:[_serverName stringByAppendingString:@"-Start"] object:nil];
+//    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(stopProfiling) name:[_serverName stringByAppendingString:@"-Stop"] object:nil];
+//#endif
+//
+//    [pool drain];
+//
+//    return self;
+//}
+//
+//- (void)startProfiling
+//{
+//    if (++_listenerCount > 1)
+//        return;
+//    JSRetainPtr<JSStringRef> profileName(Adopt, JSStringCreateWithUTF8CString([_serverName UTF8String]));
+//    JSStartProfiling(0, profileName.get());
+//}
+//
+//- (void)stopProfiling
+//{
+//    if (!_listenerCount || --_listenerCount > 0)
+//        return;
+//    JSRetainPtr<JSStringRef> profileName(Adopt, JSStringCreateWithUTF8CString([_serverName UTF8String]));
+//    JSEndProfiling(0, profileName.get());
+//}
+//
+//@end
 
 namespace JSC {
 
 void startProfilerServerIfNeeded()
 {
-    [ProfilerServer sharedProfileServer];
+//    [ProfilerServer sharedProfileServer];
 }
 
 } // namespace JSC
