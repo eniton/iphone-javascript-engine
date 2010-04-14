eefe31c52d5d5c210b5bb4b64b993803ac6371fb
Wolfgang Knebel <wknebel@numberfour.eu>
2010-04-14 16:32:37 +0200

    [PLATFORM-272] - Evaluate JavaScriptCore-525 engine
    Change:
    - file to include the generated grammar source (if not building with AllInOneFile)

---
+++ parser/include_grammar.cpp
@@ -0,0 +1,11 @@
+/*
+ *  include_grammar.cpp
+ *  JSEngine
+ *
+ *  Created by Wolfgang Knebel on 4/13/10.
+ *  Copyright 2010 NumberFour AG. All rights reserved.
+ *
+ */
+
+#include "Grammar.cpp"
+
